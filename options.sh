f8a95b8392d89c2115705e595df10cf2d65e8f2f
fcbyk <731240932@qq.com>
2024-05-31 15:27:45 +0800

    code: 更新options.sh

--- options.sh
+++ options.sh
@@ -1,3 +1,5 @@
+# 以下为git命令的选项
+
 # 显示 Git 版本信息
 git -v
 git --version
@@ -16,3 +18,12 @@ git --exec-path
 # 当你安装了多个不同版本的Git，并且希望切换使用其中一个版本时
 # 可以使用--exec-path选项来指定Git可执行文件的路径。
 git --exec-path=/path/to/new/executable/path
+
+# 显示帮助信息
+git -h
+git --help
+
+
+
+
+
