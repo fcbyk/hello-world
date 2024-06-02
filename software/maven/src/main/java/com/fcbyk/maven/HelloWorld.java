4cbc5bae4bfb7b40a7348aeb5fc4b3489d389cc7
fcbyk <731240932@qq.com>
2024-06-02 15:31:56 +0800

    code(maven): hello maven

---
+++ software/maven/src/main/java/com/fcbyk/maven/HelloWorld.java
@@ -0,0 +1,8 @@
+package com.fcbyk.maven;
+
+public class HelloWorld {
+
+    public static void main(String[] args){
+        System.out.print("Hello World");
+    }
+}
---
+++ software/maven/src/test/java/com/fcbyk/maven/Test.java
@@ -0,0 +1,9 @@
+package com.fcbyk.maven;
+
+public class Test {
+
+    @org.junit.Test
+    public void test_demo(){
+        System.out.print("hello test");
+    }
+}
