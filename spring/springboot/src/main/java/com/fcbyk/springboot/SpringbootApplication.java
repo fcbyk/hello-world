80c938ceba3971410512d24235a8d6216de4b1c5
fcbyk <731240932@qq.com>
2024-06-03 19:06:36 +0800

    code(springboot): 设置不显示Banner和简化日志输出

--- spring/springboot/src/main/java/com/fcbyk/springboot/SpringbootApplication.java
+++ spring/springboot/src/main/java/com/fcbyk/springboot/SpringbootApplication.java
@@ -1,13 +1,28 @@
 package com.fcbyk.springboot;
 
+import org.springframework.boot.Banner;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import java.util.Collections;
+
 @SpringBootApplication
-public class SpringbootApplication {
+public class SpringbootApplication implements CommandLineRunner {
 
     public static void main(String[] args) {
-        SpringApplication.run(SpringbootApplication.class, args);
+        SpringApplication app = new SpringApplication(SpringbootApplication.class);
+
+        // 设置不显示Banner和简化日志输出
+        app.setBannerMode(Banner.Mode.OFF);
+        app.setDefaultProperties(Collections.singletonMap("logging.level.root", "ERROR"));
+
+        app.run(args);
     }
 
+    @Override
+    public void run(String... args) throws Exception {
+        System.out.println(" Web server powered by SpringBoot v3.3");
+        System.out.println(" ➜  URL: http://localhost:8080/");
+    }
 }
