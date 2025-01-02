56f0c52a805c843c6ecd68b25d58f46014161cad
fcbyk <731240932@qq.com>
2025-01-02 14:58:52 +0800

    chore: 新增history目录，TimelineData类型，用于保存时间线数据

---
+++ history/c.ts
@@ -0,0 +1,79 @@
+
+export default [
+    {
+      date: '1960',
+      title: 'Algol 60 语言发布',
+      description: 'Algol 60 语言作为 C 语言的间接祖先，提出了结构化编程的理念，影响了后续许多编程语言的设计。',
+    },
+    {
+      date: '1967',
+      title: 'BCPL 开发',
+      description: 'Martin Richards 开发了 BCPL（Basic Combined Programming Language），一种简化的编程语言，为 C 语言的设计提供了重要灵感。',
+    },
+    {
+      date: '1970',
+      title: 'B 语言开发',
+      description: 'Ken Thompson 基于 BCPL 开发了 B 语言，用于 Unix 操作系统的早期开发，B 语言的设计思想直接影响了 C 语言的创建。',
+    },
+    {
+      date: '1972',
+      title: 'C 语言诞生',
+      description: 'Dennis Ritchie 在贝尔实验室基于 B 语言开发了 C 语言，主要目标是为 Unix 操作系统提供高效的编程工具。C 语言的简洁性和强大功能为后续的系统编程奠定了基础。',
+    },
+    {
+      date: '1973',
+      title: 'C 语言重写 Unix',
+      description: 'Unix 操作系统的大部分被用 C 语言重写，这一事件进一步巩固了 C 语言在操作系统开发中的地位，并展示了其作为系统级编程语言的强大能力。',
+    },
+    {
+      date: '1978',
+      title: '《The C Programming Language》发布',
+      description: 'Brian Kernighan 和 Dennis Ritchie 合著的《The C Programming Language》（K&R 版 C 语言手册）发布，这本书成为 C 语言的权威指南，也标志着 C 语言成为一种广泛使用的编程语言。',
+    },
+    {
+      date: '1983',
+      title: 'ANSI C 标准委员会成立',
+      description: '美国国家标准协会（ANSI）成立了 C 语言标准委员会（X3J11），旨在规范 C 语言的实现，为后续的标准化打下基础。',
+    },
+    {
+      date: '1989',
+      title: 'ANSI C（C89）发布',
+      description: 'ANSI 发布了第一个 C 语言标准，称为 ANSI C（或 C89）。C89 引入了许多规范化特性，确保了 C 语言的跨平台兼容性并成为现代 C 语言的基础。',
+    },
+    {
+      date: '1990',
+      title: 'ISO C（C90）发布',
+      description: '国际标准化组织（ISO）采纳了 ANSI C 标准，发布了等效的 ISO C 标准（C90），该标准进一步提高了 C 语言的全球一致性。',
+    },
+    {
+      date: '1995',
+      title: 'C95 发布',
+      description: 'ISO 发布了一个小型修订版 C95，为 C90 增加了一些小的改进，例如对宽字符支持的增强，使得 C 语言更适应国际化需求。',
+    },
+    {
+      date: '1999',
+      title: 'C99 发布',
+      description: 'ISO 发布了 C99 标准，增加了许多新特性，包括内联函数、变长数组和对浮点数支持的增强，推动 C 语言进入了新的发展阶段。',
+    },
+    {
+      date: '2011',
+      title: 'C11 发布',
+      description: 'ISO 发布了 C11 标准，进一步改进了语言的功能和安全性。C11 强调多核处理支持、原子操作和内存模型，使得 C 语言在现代硬件上更为高效。',
+    },
+    {
+      date: '2007-2011',
+      title: 'C11 开发',
+      description: 'C11 的开发过程中，吸收了 C++ 和 Java 等语言的经验，同时仍保持 C 语言的简洁性和高效性，增加了对多线程和并发编程的支持。',
+    },
+    {
+      date: '2018',
+      title: 'C17（C18）发布',
+      description: 'ISO 发布了 C17 标准（也称为 C18），它主要是对 C11 标准的小修订，修复了一些技术问题，并加强了语言的稳定性和兼容性。',
+    },
+    {
+      date: '2023',
+      title: 'C23 发布',
+      description: 'C23 标准发布，引入更多现代化特性，增强语言的可用性和兼容性，进一步提高 C 语言在现代开发环境中的竞争力，尤其是在嵌入式和高性能计算领域。',
+    },
+  ] as TimelineData;
+  
---
+++ history/index.d.ts
@@ -0,0 +1,5 @@
+declare type TimelineData = {
+    date: string;
+    title: string;
+    description: string;
+}[]
