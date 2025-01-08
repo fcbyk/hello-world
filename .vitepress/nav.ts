2f7dc3cadd74c4e3209ce44e842c2831575ffcf2
fcbyk <731240932@qq.com>
2025-01-08 10:28:34 +0800

    docs: 新增 操作系统 部分

--- .vitepress/nav.ts
+++ .vitepress/nav.ts
@@ -13,5 +13,26 @@ export default [
                 link:"/backend/c"
             }
         ]
+    },
+    {
+        text:"408",
+        items:[
+            {
+                text:"计算机网络",
+                link:"."
+            },
+            {
+                text:"计算机组成原理",
+                link:"."
+            },
+            {
+                text:"操作系统",
+                link:"/os/"
+            },
+            {
+                text:"数据结构",
+                link:"."
+            },
+        ]
     }
 ] as DefaultTheme.NavItem[]
--- .vitepress/sidebar.ts
+++ .vitepress/sidebar.ts
@@ -2,6 +2,7 @@ import { DefaultTheme } from "vitepress";
 import index from "../docs/sidebar"
 import backend from "../docs/backend/sidebar"
 import friontend from "../docs/frontend/sidebar"
+import os from "../docs/os/sidebar"
 
 export default {
     "/":index,
@@ -17,6 +18,13 @@ export default {
             text:"WEB前端",
             base:"/frontend/",
             items:friontend
+        }
+    ],
+    "/os/":[
+        {
+            text:"操作系统",
+            base:"/os/",
+            items:os
         }
     ]
 } as DefaultTheme.Sidebar
